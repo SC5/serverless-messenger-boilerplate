ced24b6f8b08f15aa0e23bf02a60d3039dbabcaf
Mikael Puittinen <mikael@iki.fi>
2016-10-17 15:52:09 +0300

    Move custom Wit.ai actions to my-wit-actions.js

--- facebook-bot/wit-ai.js
+++ facebook-bot/wit-ai.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const Wit = require('node-wit').Wit;
-const moment = require('moment');
-const weather = require('./weather');
+const myActions = require('./my-wit-actions');
 
 /**
  * Handles wit.ai integration
@@ -12,47 +11,21 @@ const init = event => new Promise((resolveMessage, rejectMessage) => {
   if (event.sender && event.sender.id && event.message && event.message.text) {
     const sessionId = `${event.id}-${event.updated}`;
     const context0 = {};
+    const actions = {
+      send: (request, response) => new Promise(() => {
+        resolveMessage(response);
+      })
+    };
+    // Copy custom actions to actions
+    Object.keys(myActions).map((value) => {
+      actions[value] = myActions[value];
+    });
+
     const client = new Wit({
       accessToken: process.env.WIT_AI_TOKEN,
-      actions: {
-        send: (request, response) => new Promise(() => {
-          resolveMessage(response);
-        }),
-        getWeather: (data) => new Promise((resolve, reject) => {
-          const context = data.context;
-          const entities = data.entities;
-
-          const missingLocation = entities.location === undefined;
-          const location = entities.location ? entities.location[0].value : null;
-          const datetime = entities.datetime ? entities.datetime[0].value : null;
-
-          if (missingLocation) {
-            const contextData = Object.assign({}, context, { missingLocation });
-            resolve(contextData);
-          } else if (datetime) {
-            weather.forecastByLocationName(location, datetime)
-              .then((weatherData) => {
-                const contextData = Object.assign({}, context, weatherData);
-                if (datetime) {
-                  Object.assign(contextData, { datetime: moment(datetime).calendar().toLowerCase() });
-                }
-                resolve(contextData);
-              })
-              .catch(reject);
-          } else {
-            weather.weatherByLocationName(location)
-              .then((weatherData) => {
-                const contextData = Object.assign({}, context, weatherData);
-                if (datetime) {
-                  Object.assign(contextData, { datetime: moment(datetime).calendar().toLowerCase() });
-                }
-                resolve(contextData);
-              })
-              .catch(reject);
-          }
-        })
-      }
+      actions: actions
     });
+
     client.runActions(sessionId, event.message.text, context0);
   } else {
     rejectMessage('wit ai failed');
