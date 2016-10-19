d1924f458e15b8e83a69680d1db9a79eb7dadee9
Mikael Puittinen <mikael@iki.fi>
2016-10-19 13:02:35 +0300

    Moved receiving message to library to ease adding e.g. SQS

--- wit-ai/handler.js
+++ wit-ai/handler.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const witAi = require('./wit-ai.js');
+const messageQueue = require('../lib/messagequeue.js');
 
 /**
  * Adds extra environmental variables
@@ -15,10 +16,15 @@ module.exports.handler = (event, context, cb) => {
   setEnvVars(event);
   if (process.env.WIT_AI_TOKEN) {
     if (event.Records && event.Records[0] && event.Records[0].Sns) {
-      const snsEvent = JSON.parse(event.Records[0].Sns.Message);
-      witAi(snsEvent)
-      .then(result => {
-        cb(null, result);
+      messageQueue.getMessage(event)
+      .then((message) => {
+        witAi(message)
+        .then(result => {
+          cb(null, result);
+        })
+        .catch(error => {
+          cb(error);
+        });
       })
       .catch(error => {
         cb(error);
