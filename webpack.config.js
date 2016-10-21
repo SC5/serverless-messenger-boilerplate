367aa28bebeec809a9f8eacbf35af4a84e3ef0fa
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-10-21 13:04:06 +0300

    adds webpack plugin

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'facebook-bot/handler': './facebook-bot/handler.js',
+    'wit-ai/handler': './wit-ai/handler.js'
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      { test: /\.json/, loader: 'json-loader' },
+      { test: /\.env/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: '.env' }
+    ])
+  ],
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  },
+};
