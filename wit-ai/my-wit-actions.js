0220dbcb2faabfcc193b86fff9f4019b4d584d42
Mikael Puittinen <mikael@iki.fi>
2016-10-22 15:44:31 +0300

    Clean up boilerplate

--- wit-ai/my-wit-actions.js
+++ wit-ai/my-wit-actions.js
@@ -2,38 +2,6 @@ const moment = require('moment');
 const weather = require('./weather');
 
 const actions = {
-  getWeather: data => new Promise((resolve, reject) => {
-    const context = data.context;
-    const entities = data.entities;
-    const missingLocation = entities.location === undefined;
-    const location = entities.location ? entities.location[0].value : null;
-    const datetime = entities.datetime ? entities.datetime[0].value : null;
-
-    if (missingLocation) {
-      const contextData = Object.assign({}, context, { missingLocation });
-      resolve(contextData);
-    } else if (datetime) {
-      weather.forecastByLocationName(location, datetime)
-        .then((weatherData) => {
-          const contextData = Object.assign({}, context, weatherData);
-          if (datetime) {
-            Object.assign(contextData, { datetime: moment(datetime).calendar().toLowerCase() });
-          }
-          resolve(contextData);
-        })
-        .catch(reject);
-    } else {
-      weather.weatherByLocationName(location)
-        .then((weatherData) => {
-          const contextData = Object.assign({}, context, weatherData);
-          if (datetime) {
-            Object.assign(contextData, { datetime: moment(datetime).calendar().toLowerCase() });
-          }
-          resolve(contextData);
-        })
-        .catch(reject);
-    }
-  }),
   getTime: data => new Promise((resolve) => {
     // Sample action for node-wit
     const contextData = {};
