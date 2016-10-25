1e483df6ebfd90b477ca2eb8468f670722019c3f
Eetu Tuomala <eetu.tuomala@sc5.io>
2016-10-25 23:00:34 +0100

    adds error handling in case of invalid action

--- wit-ai/wit-ai.js
+++ wit-ai/wit-ai.js
@@ -11,9 +11,10 @@ const session = require('../lib/session.js');
 const init = event => new Promise((resolveMessage, rejectMessage) => {
   if (event.sender && event.sender.id && event.message && event.message.text) {
     const sessionId = `${event.id}-${event.updated}`;
+    const recipient = { id: event.sender.id };
+
     const actions = {
       send: (request, response) => new Promise((resolve) => {
-        const recipient = { id: event.sender.id };
         resolveMessage({ message: response, recipient });
         resolve();
       }),
@@ -34,7 +35,15 @@ const init = event => new Promise((resolveMessage, rejectMessage) => {
 
     client.runActions(sessionId, event.message.text, Object.assign({}, event.context))
       .then(ctx => session.writeSession({ id: event.id, updated: event.updated, context: ctx }))
-      .catch(error => rejectMessage(error));
+      .catch((error) => {
+        console.log('wit.ai error', error);
+        resolveMessage({
+          message: {
+            text: error
+          },
+          recipient
+        });
+      });
   } else {
     rejectMessage(`Missing sender / message in ${JSON.stringify(event, null, 2)}`);
   }
